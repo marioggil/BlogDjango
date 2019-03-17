66b66e1f788519a753a7793254b1d0b3437503c0
marioggil <marioggil@gmail.com>
2019-03-16 22:27:39 -0500

    Arreglos varios en Css y archivos varios

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('blog', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit , name='post_edit'),
@@ -10,5 +10,5 @@ urlpatterns = [
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('about/', views.AboutPageView.as_view(), name='about'),
-    path('home/', views.HomePageView.as_view(), name='home'),
+    path('', views.HomePageView.as_view(), name='home'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%z%of&@=(fg6946k3*^h%#xvaobyo6*7vw^cwt)vs)7*q+zsuq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ".pythonanywhere.com", "marioggil.pythonanywhere.com"]
+ALLOWED_HOSTS = ['127.0.0.1', ".pythonanywhere.com"]
 
 
 # Application definition
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pygmentify',
-    'latexify',
     'blog',
 ]
 
